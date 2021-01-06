047f4869853f2ffeebd21400be97fbe9a410a5a9
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-05 21:21:26 -0300

    add LoginPage layout

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import '../components/components.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Login Page"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LoginHeader(),
+            HeadLine1(text: 'Login'),
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                            Icons.email,
+                            color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 32.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Senha',
+                            icon: Icon(
+                                Icons.lock,
+                                color: Theme.of(context).primaryColorLight,
+                            )
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: (){},
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    FlatButton.icon(
+                        onPressed: (){},
+                        icon: Icon(Icons.person),
+                        label: Text('Criar Conta')
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+
