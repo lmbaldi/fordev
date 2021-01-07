559622a793f660ce4de06886d7f9770b41bc8dbb
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-07 18:50:40 -0300

    ensure LoginPage presents no  error if password is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -97,9 +97,29 @@ void main() {
   testWidgets('Should present error if password is invalid',
       (WidgetTester tester) async {
     await loadPage(tester);
-    passwordErrorController.add('any error');
+    passwordErrorController.add(null);
     await tester.pump();
     expect(find.text('any error'), findsOneWidget);
+  });
+
+  testWidgets('Should present no error if password is valid',
+          (WidgetTester tester) async {
+        await loadPage(tester);
+        passwordErrorController.add(null);
+        await tester.pump();
+        final passwordTextChildren = find.descendant(
+            of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+        expect(passwordTextChildren, findsOneWidget);
+      });
+
+  testWidgets('Should present no error if password is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+    passwordErrorController.add('');
+    await tester.pump();
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(passwordTextChildren, findsOneWidget);
   });
 }
 
