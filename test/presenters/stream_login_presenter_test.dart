47dc8b8f13562de95ddb19e8e5b10a2ca892d4c3
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-09 16:31:35 -0300

    move Authentication mock to a helper method

--- test/presenters/stream_login_presenter_test.dart
+++ test/presenters/stream_login_presenter_test.dart
@@ -6,53 +6,60 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 
 abstract class Validation {
-  String validate({@required String field, @required String  value});
+  String validate({@required String field, @required String value});
 }
 
 class LoginState {
   String emailError;
 }
 
-class StreamLoginPresenter{
+class StreamLoginPresenter {
   final Validation validation;
   final _controller = StreamController<LoginState>.broadcast();
   var _state = LoginState();
 
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
-  void validateEmail(String email){
+  void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
   }
 }
 
-class ValidationSpy extends Mock implements Validation{}
+class ValidationSpy extends Mock implements Validation {}
 
-void main(){
-
+void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
 
-  setUp((){
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed("value")));
+
+  void mockValidation({String field, String value}){
+    mockValidationCall(field).thenReturn(value);
+  }
+
+  setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    //retornar sucesso por padrao, quando esta sem parametro
+    mockValidation();
   });
 
-  test('Should call Validation with correct mail', (){
+  test('Should call Validation with correct mail', () {
     sut.validateEmail(email);
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
 
-  test('Should emit email error if validation fails', (){
-    when(validation.validate(field: anyNamed('field'), value: anyNamed("value")))
-    .thenReturn('error');
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
     expectLater(sut.emailErrorStream, emits('error'));
     sut.validateEmail(email);
   });
-
 }
-
