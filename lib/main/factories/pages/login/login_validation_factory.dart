c5ec56c97689fae91427005c29d2062ccfaad51e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-12 21:28:20 -0300

    ensure LoginValidationsFactory returns correct validations

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,10 +1,15 @@
-import 'package:fordev/presentation/protocols/validation.dart';
+import 'package:fordev/validation/protocols/protocols.dart';
+import 'package:fordev/presentation/protocols/protocols.dart';
 import 'package:fordev/validation/validators/validators.dart';
 
 Validation makeLoginValidation() {
-  return ValidationComposite([
+  return ValidationComposite(makeLoginValidations());
+}
+
+List<FieldValidation> makeLoginValidations(){
+  return [
     RequiredFieldValidation('email'),
     EmailValidation('email'),
     RequiredFieldValidation('password')
-  ]);
+  ];
 }
---
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/main/factories/factories.dart';
+import 'package:fordev/validation/validators/validators.dart';
+
+void main(){
+  test('Should return the correct validations', (){
+    final validations = makeLoginValidations();
+    expect(validations,[
+      RequiredFieldValidation('email'),
+      EmailValidation('email'),
+      RequiredFieldValidation('password')
+    ]);
+  });
+}
