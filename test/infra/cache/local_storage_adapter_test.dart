7109d1ab64f337799cd545eec2582a713edd862e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-19 22:35:27 -0300

    ensure LocalStoreAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -7,7 +7,9 @@ import 'package:fordev/data/cache/cache.dart';
 
 class LocalStoreAdapter implements SaveSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
+
   LocalStoreAdapter({@required this.secureStorage});
+
   Future<void> saveSecure(
       {@required String key, @required String value}) async {
     await secureStorage.write(key: key, value: value);
@@ -22,15 +24,29 @@ void main() {
   String key;
   String value;
 
-  setUp((){
+  setUp(() {
     secureStorage = FlutterSecureStorageSpy();
     sut = LocalStoreAdapter(secureStorage: secureStorage);
     key = faker.lorem.word();
     value = faker.guid.guid();
   });
 
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
   test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  //prevenir falha da biblioteca se ela mudar
+  test('Should throw if save secure throws', () async {
+    //mockar a biblioteca do  FlutterSecureStorage pra retornar uma excecao
+    mockSaveSecureError();
+    final future = sut.saveSecure(key: key, value: value);
+    //compara apenas o tipo se retorna uma excecao
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
