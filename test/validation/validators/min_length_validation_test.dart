d4298ab1bd89e79bf5f7e29d57a5af7e5baefa72
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-06 16:48:25 -0300

    ensure MinLengthValidation returns error if value is less than min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -1,12 +1,17 @@
-import 'package:fordev/presentation/protocols/protocols.dart';
-import 'package:fordev/validation/protocols/protocols.dart';
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+import 'package:fordev/validation/protocols/protocols.dart';
+import 'package:fordev/presentation/protocols/protocols.dart';
 
 class MinLengthValidation implements FieldValidation{
   final String field;
   final int size;
 
-  MinLengthValidation({this.field, this.size});
+  MinLengthValidation({
+    @required this.field,
+    @required this.size
+  });
   
   ValidationError validate(String value){
     return ValidationError.invalidField;
@@ -28,5 +33,11 @@ void main(){
   test('Should return error if value is null', (){
     expect(sut.validate(null), ValidationError.invalidField);
   });
+
+  test('Should return error if value is less than min size', (){
+    expect(sut.validate(faker.randomGenerator.string(4, min: 1)), ValidationError.invalidField);
+  });
+
+
 }
 
