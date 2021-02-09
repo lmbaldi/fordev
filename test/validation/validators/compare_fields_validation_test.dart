13bb96c9ea9a323c02ed2caf6ebfacbc93d08751
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-09 19:25:12 -0300

    ensure CompareFieldsValidation return null if values are equal

--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -11,8 +11,12 @@ void main(){
      sut = CompareFieldsValidation(field: 'any_field', valueToCompare: 'any_value');
   });
 
-  test('Should return error if value is not equal', (){
+  test('Should return error if values are not equal', (){
     expect(sut.validate('wrong_value'), ValidationError.invalidField);
+  });
+
+  test('Should return null if value are equal', (){
+    expect(sut.validate('any_value'), null);
   });
 
 
