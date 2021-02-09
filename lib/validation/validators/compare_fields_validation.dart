0510170d4d2287e2ce76367fbad42b216d0ac58f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-09 19:22:07 -0300

    ensure CompareFieldsValidation return error if values are not equal

---
+++ lib/validation/validators/compare_fields_validation.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+import '../../validation/protocols/protocols.dart';
+import '../../presentation/protocols/protocols.dart';
+
+class CompareFieldsValidation implements FieldValidation {
+  final String field;
+  final String valueToCompare;
+
+  CompareFieldsValidation({
+    @required this.field,
+    @required this.valueToCompare
+  });
+
+  ValidationError validate(String value){
+    return   ValidationError.invalidField;
+  }
+}
