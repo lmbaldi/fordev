76b7426e426940430b880ed088b747c9c43c68ba
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-09 21:10:44 -0300

    add invalid cases test for CompareFieldsValidation

--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -11,13 +11,19 @@ void main(){
      sut = CompareFieldsValidation(field: 'any_field', fieldToCompare: 'other_field');
   });
 
+  test('Should return null on invalid cases', (){
+    expect(sut.validate({ 'any_field': 'any_value'}), null);
+    expect(sut.validate({ 'other_field': 'other_value'}), null);
+    expect(sut.validate({}), null);
+  });
+
   test('Should return error if values are not equal', (){
     final formData = { 'any_field': 'any_value', 'other_field': 'other_value'};
     expect(sut.validate(formData), ValidationError.invalidField);
   });
 
   test('Should return null if value are equal', (){
-    final formData = { 'any_field': 'any_value', 'other_field': 'other_value'};
+    final formData = { 'any_field': 'any_value', 'other_field': 'any_value'};
     expect(sut.validate(formData), null);
   });
 
