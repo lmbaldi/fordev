c6afff1af1cb9a4855b6658f5ea4b1b8a67ce056
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-11 20:50:26 -0300

    ensure HttpAdapter throws BadRequest on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -174,5 +174,17 @@ void main() {
       expect(response, null);
     });
 
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400, body: '');
+      final future = sut.request(url: url, method: 'GET');
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400);
+      final future = sut.request(url: url, method: 'GET');
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
   });
 }
