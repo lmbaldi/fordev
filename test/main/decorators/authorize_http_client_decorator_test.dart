548ecdd6743645590f6d5327d0016f049d2b4d23
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-18 15:18:19 -0300

    ensure Decorator adds new header if header is null

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,40 +1,70 @@
+import 'dart:io';
+
 import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:fordev/data/http/http.dart';
 import 'package:fordev/data/cache/cache.dart';
 
-class AuthorizeHttpClientDecorator{
-
+class AuthorizeHttpClientDecorator {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
+  final HttpClient decoratee;
 
-  AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
+  AuthorizeHttpClientDecorator({
+    @required this.fetchSecureCacheStorage,
+    @required this.decoratee
+  });
 
   Future<void> request({String url, String method, Map body, Map headers}) async {
-    await fetchSecureCacheStorage.fetchSecure('token');
+    final token =  await fetchSecureCacheStorage.fetchSecure('token');
+    final authorizedHeaders = {'x-access-token': token};
+    await decoratee.request(url: url, method: method, body: body, headers:  authorizedHeaders);
   }
 }
+
 //classe mock criada porque nao se pode criar uma instancia de uma inferface
-class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage{}
+class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
+class HttpClientSpy extends Mock implements HttpClient {}
 
-void main(){
-
+void main() {
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
   AuthorizeHttpClientDecorator sut;
+  HttpClientSpy httpClient;
+  Map body;
   String url;
   String method;
-  Map body;
+  String token;
 
-  setUp((){
+  void mockToken() {
+    token = faker.guid.guid();
+    when(fetchSecureCacheStorage.fetchSecure(any)).thenAnswer((_) async => token );
+  }
+
+  setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
-    sut = AuthorizeHttpClientDecorator(fetchSecureCacheStorage:fetchSecureCacheStorage);
+    httpClient = HttpClientSpy();
+    sut = AuthorizeHttpClientDecorator(
+        fetchSecureCacheStorage: fetchSecureCacheStorage,
+        decoratee: httpClient
+    );
     url = faker.internet.httpUrl();
     method = faker.randomGenerator.string(10);
-    body = {'any_key' : 'any_value' };
+    body = {'any_key': 'any_value'};
+    mockToken();
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () async {
     await sut.request(url: url, method: method, body: body);
     verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+
+  test('Should call decoratee with access token on header', () async {
+    await sut.request(url: url, method: method, body: body);
+    verify(httpClient.request(
+        url: url,
+        method: method,
+        body: body,
+        headers: {'x-access-token': token})).called(1);
   });
 }
