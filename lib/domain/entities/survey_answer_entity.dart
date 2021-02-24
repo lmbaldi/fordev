ef2259cddc948b607d93e0be7a57a35c186d1071
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-24 19:53:35 -0300

    add SurveyResult Entity

---
+++ lib/domain/entities/survey_answer_entity.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+
+class SurveyAnswerEntity{
+
+  final String image;
+  final String answer;
+  final bool isCurrentAnswer;
+  final int percent;
+
+  SurveyAnswerEntity({
+    this.image,
+    @required this.answer,
+    @required this.isCurrentAnswer,
+    @required this.percent,
+  });
+
+}
---
+++ lib/domain/entities/survey_result_entity.dart
@@ -0,0 +1,16 @@
+import 'package:meta/meta.dart';
+import './entities.dart';
+
+class SurveyResultEntity{
+
+  final String surveyId;
+  final String question;
+  final List<SurveyAnswerEntity> answers;
+
+  SurveyResultEntity({
+    @required this.surveyId,
+    @required this.question,
+    @required this.answers,
+  });
+
+}
