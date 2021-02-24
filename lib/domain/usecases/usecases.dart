8e49cad176565a0fc7a01f6826b81c27140e9753
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-24 19:59:18 -0300

    LoadSurveyResult UseCase

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,5 +1,6 @@
-export './authentication.dart';
-export './save_current_account.dart';
-export './load_current_account.dart';
 export './add_account.dart';
+export './authentication.dart';
 export './load_surveys.dart';
+export './load_survey_result.dart';
+export './load_current_account.dart';
+export './save_current_account.dart';
