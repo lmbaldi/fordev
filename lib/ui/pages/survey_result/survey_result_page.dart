aff7e466d755cacca4527d78346879ff277c936c
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-25 15:16:06 -0300

    add SurveyResult Layout

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -3,15 +3,73 @@ import 'package:flutter/material.dart';
 import '../../helpers/helpers.dart';
 
 class SurveyResultPage extends StatelessWidget {
-
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(R.string.surveys),),
-      body: Text('OK'),
+      appBar: AppBar(
+        title: Text(R.string.surveys),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Container(
+              padding:
+                  EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+              decoration: BoxDecoration(
+                color: Theme.of(context).disabledColor.withAlpha(90),
+              ),
+              child: Text('Qual e o seu framework web preferido?'),
+            );
+          }
+          return Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(14.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.network(
+                      'http://fordevs.herokuapp.com/static/img/logo-angular.png',
+                      width: 40,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Angular',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '100%',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Theme.of(context).highlightColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 1,
+              ),
+            ],
+          );
+        },
+        itemCount: 4,
+      ),
     );
   }
 }
-
-
-
