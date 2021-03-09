963ce9b38bac4902593c007a771b7ca10d6c7387
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-08 21:18:31 -0300

    remove duplication

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 
 import '../../ui/pages/pages.dart';
 import '../../ui/helpers/helpers.dart';
+import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 import '../mixins/mixins.dart';
@@ -24,44 +25,26 @@ class GetxSurveyResultPresenter extends GetxController
   Stream<SurveyResultViewModel> get surveyResultStream => _surveyResult.stream;
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult = await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers.map((answer) => SurveyAnswerViewModel(
-          image: answer.image,
-          answer: answer.answer,
-          percent: '${answer.percent}%',
-          isCurrentAccountAnswer: answer.isCurrentAccountAnswer
-        )).toList()
-      );
-
-    } on DomainError catch(error) {
-      if(error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      }else{
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
 
   Future<void> save({@required String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
-      final surveyResult = await saveSurveyResult.save(answer: answer);
+      final surveyResult = await action();
       _surveyResult.value = SurveyResultViewModel(
           surveyId: surveyResult.surveyId,
           question: surveyResult.question,
           answers: surveyResult.answers
               .map((answer) => SurveyAnswerViewModel(
-                  image: answer.image,
-                  answer: answer.answer,
-                  percent: '${answer.percent}%',
-                  isCurrentAccountAnswer: answer.isCurrentAccountAnswer))
+              image: answer.image,
+              answer: answer.answer,
+              percent: '${answer.percent}%',
+              isCurrentAccountAnswer: answer.isCurrentAccountAnswer))
               .toList());
     } on DomainError catch (error) {
       if (error == DomainError.accessDenied) {
