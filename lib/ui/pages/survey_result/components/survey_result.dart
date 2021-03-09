e60095edf9511ad63a6a1642b51c102426018e1c
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-09 09:51:07 -0300

    prevent current answer click

--- lib/ui/pages/survey_result/components/survey_result.dart
+++ lib/ui/pages/survey_result/components/survey_result.dart
@@ -17,9 +17,13 @@ class SurveyResult extends StatelessWidget {
         if (index == 0) {
           return SurveyHeader(viewModel.question);
         }
+        final answer = viewModel.answers[index - 1];
         return GestureDetector(
-          onTap: () => onSave(answer: viewModel.answers[index - 1].answer),
-          child: SurveyAnswer(viewModel.answers[index - 1]),);// subtrai um porque o primeiro elemento do array e a pergunta
+          onTap: () => answer.isCurrentAccountAnswer
+              ? null
+              : onSave(answer: answer.answer),
+          child: SurveyAnswer(answer),
+        );// subtrai um porque o primeiro elemento do array e a pergunta
       },
       itemCount: viewModel.answers.length + 1,//somar um por causa da pergunta
     );
